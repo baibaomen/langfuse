e0815e63ec0366a1ba8f4431eeccbd9214fe39ea
baibaomen <baibaomen@gmail.com>
2024-02-16 12:23:09 +0800

    Add keycloak

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,17 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/src/server/auth";
+import Providers from "next-auth/providers";
+import KeycloakProvider from "next-auth/providers/keycloak";
 
-export default NextAuth(authOptions);
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_ID ?? "",
+      clientSecret: process.env.KEYCLOAK_SECRET ?? "",
+      issuer: process.env.KEYCLOAK_ISSUER,
+    }),
+    // Add more providers as needed
+  ],
+  // Add custom configuration as needed
+});
